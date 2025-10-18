26a8004c2a76faa27b982a99bd2239eaa5656621
Diego24110202 <a24110202@live.ceti.mx>
2025-10-18 01:35:04 +0000

    Practica 4

--- src/ejemplo.cpp
+++ src/ejemplo.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <string>
+#include<sstream>
+#include <thread>
+#include <chrono>
 using namespace std;
  
 #include <ftxui/component/component.hpp>
@@ -6,6 +10,17 @@ using namespace std;
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 using namespace ftxui;
+
+string nave=
+string("     ___    \n" )+    
+string(" ___/   \___ \n")+
+string("/   '---'   \ \n")+ 
+string("'--_______--' \n")+
+string("     / \      \n")+  
+string("    /   \      \n")+ 
+string("    /\O/\      \n")+ 
+string("    / | \      \n")+  
+string("    // \\     \n")+ 
 
 int main(int argc, char const *argv[])
 {
@@ -17,13 +32,35 @@ int main(int argc, char const *argv[])
     int x = 0;
     int y = 0;
 
-    auto pixel = pantalla.PixelAt(1,1);
-    pixel.blink = true;
-    pixel.background_color = Color::Blue;
-    pixel.foreground_color = Color::red;
-    pixel.character = "A";
+    stringstream ss(nave);
 
+    while (true)
+{
+    X++;
+
+    stringstream ss(nave);
+    string linea;
+    int nolinea = 0;
+    while (getline(ss, linea))
+    {
+    int columna = 0;
+    for (auto &&letra : linea)
+    {
+        //obtener referencia al pixely modificarlo
+        auto &pixel = pantalla.PixelAt(x + columna,y + nolinea);
+        pixel.character= letra;
+        columna++;
+
+
+}
+      nolinea++;
+}
+// Imprimir la pantalla
+    pantalla.ResetPosition();
     pantalla.Print();
+    pantalla.Clear();
+    this_thread::sleep_for(chrono::miliseconds(100));
+    cout<<pantalla.ResetPosition();
 
     return 0;
 }
